0032b64e45529f2040b6f8a0ed9f409810d6aa72
CesarRawr <rawrcesar13@gmail.com>
2021-07-25 10:57:21 -0500

    rwr

---
+++ pages/fail/index.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Berdenz redirect</title>
+        <meta name="description" content="Página que te redirecciona al Whatsapp de tu sucursal Berdenz mas cercana" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.loader}>Loading...</div>
+        <span>asdf</span>
+      </main>
+    </div>
+  )
+}
