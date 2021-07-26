32cb4542a05a2f3c3d69e45e8e1b100681c6234d
CesarRawr <rawrcesar13@gmail.com>
2021-07-26 18:18:55 -0500

    rwr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,24 @@
 import { useEffect, useState } from 'react';
 import Head from 'next/head';
+import { useLoadScript, DistanceMatrixService } from '@react-google-maps/api';
 import styles from '../styles/Home.module.css';
 
-/* Key key=AIzaSyAxrCURezqs3zORD2Xcjmg5YKKUcAFl70A*/
-/* Plaza museo coords 19.548700194914215, -96.9315025473798*/
-/* Martires de chicago coords 19.508979105975133, -96.90683500320357 */
-/* José Ruiz coords 19.55817402214415, -96.91246640498656 */
-/* https://maps.googleapis.com/maps/api/distancematrix/json?origins=enc:aaivBp|rnQ~vF}xCosHhb@:&destinations=19.509692062756834, -96.90727354434168&key=AIzaSyAxrCURezqs3zORD2Xcjmg5YKKUcAFl70A */
-
 export default function Home() {
-  const [msg, setMsg] = useState<string>('Cargando...');
-
   // Configuración para el modal que pide permiso de ubicación
   const config: any = {
     enableHighAccuracy: true, 
     maximumAge: 30000, 
     timeout: 27000,
   };
+
+  const [msg, setMsg] = useState<string>('Cargando...');
+  const [coords, setCoords] = useState<Coords>();
+  const [isPermission, setPermission] = useState<boolean>(false);
+  
+  // Espera a que la api de google haya sido cargada
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyAo-HC19aPSYGm0mt826eoCliQkfNAueCo',
+  });
 
   // Hook inicial que revisa si existe el servicio de ubicación en el navegador
   useEffect(() => {
@@ -31,19 +33,17 @@ export default function Home() {
   // Función que se ejecuta si existe permiso de ubicación
   const onSucccess: any = (position: any) => {
     setMsg('Redirigiendolo a la sucursal mas cercana...');
-    console.log(position.coords.latitude, position.coords.longitude);
-    const url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=enc:aaivBp|rnQ~vF}xCosHhb@:&destinations=19.509692062756834, -96.90727354434168&key=AIzaSyAxrCURezqs3zORD2Xcjmg5YKKUcAFl70A';
-    
-    fetch(url)
-    .then((res: any) => res.json())
-    .then((json: any) => console.log(json))
-    .catch((err: any) => console.log(err));
+    setCoords({
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    });
+    setPermission(true);
   }
   
   // Función de error al pedir ubicación
   const onError: any = (err: any) => {
     console.log(err);
-    setMsg("Se denegaron los permisos de ubicación, redirigiendolo a las posibles ubicaciones...");
+    setMsg("Ocurrió un error al obtener su ubicación, mostrandole las sucursales disponibles");
   }
 
   return (
@@ -57,6 +57,34 @@ export default function Home() {
         <div className={styles.loader}>Loading...</div>
         <span>{msg}</span>
       </main>
+      {
+        isLoaded && isPermission && (
+          <DistanceMatrixService 
+            options={{
+              destinations: [coords],
+              origins: [
+                { lat: 19.548700194914215, lng: -96.9315025473798 },
+                { lat: 19.508979105975133, lng: -96.90683500320357 },
+                { lat: 19.55817402214415, lng: -96.91246640498656 }
+              ],
+              travelMode: "DRIVING",
+            }}
+            callback={(res) => {
+              console.log("RESPONSE", res);
+            }}
+          />
+        )
+      }
     </div>
   )
 }
+
+interface Coords {
+  lat: number;
+  lng: number;
+}
+
+/* Key AIzaSyAo-HC19aPSYGm0mt826eoCliQkfNAueCo*/
+/* Plaza museo coords 19.548700194914215, -96.9315025473798*/
+/* Martires de chicago coords 19.508979105975133, -96.90683500320357 */
+/* José Ruiz coords           19.55817402214415,  -96.91246640498656 */
