61954b9322325c3713702a2132fe6ea01accbd39
CesarRawr <rawrcesar13@gmail.com>
2021-07-26 20:56:57 -0500

    rwr

--- services/redirect.ts
+++ services/redirect.ts
@@ -1,6 +1,14 @@
 const RedirectService = {
-	redirect: (rows: any) => {
-		console.log(rows);
+	redirect: (rows: any[]) => {
+		let nearest: number = 0;
+
+		for (let i = 0; i<rows.length; i++) {
+			if (rows[i].elements[0].distance['value'] < rows[nearest].elements[0].distance['value']) {
+				nearest = i;
+			}
+		}
+
+		console.log(nearest);
 	},
 };
 
