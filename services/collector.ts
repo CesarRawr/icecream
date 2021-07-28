b15b7e1c561d6e628b64ba895e681cb3583b1aa3
CesarRawr <rawrcesar13@gmail.com>
2021-07-27 19:01:33 -0500

    Refactoring

--- services/collector.ts
+++ services/collector.ts
@@ -5,7 +5,7 @@ const CollectorService = {
 		let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
 		let mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d);
 		let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
-		let ho = new Intl.DateTimeFormat('en', { hour12: true }).format(d);
+		let ho = new Intl.DateTimeFormat('en', { hour: "2-digit" }).format(d);
 		let min = new Intl.DateTimeFormat('en', { minute: "2-digit" }).format(d);
 		let seg = new Intl.DateTimeFormat('en', { second: "2-digit" }).format(d);
 
